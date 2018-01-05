1235c1b5d7af3a3491e26516400d94ec9204eab5
szabolcs.szrenko <szabolcs.szrenko@incepteam.com>
2018-01-05 17:59:03 +0100

    ConvertToEquirecTangular

--- grabber/Program.cs
+++ grabber/Program.cs
@@ -14,7 +14,9 @@ namespace grabber
     {
         static void Main(string[] args)
         {
-            var doc = new HtmlDocument();
+            Bitmap bmp = (Bitmap)Image.FromFile("pano3.jpg");
+            ConvertToEquirecTangular(bmp, 3200, 2400);
+            /*var doc = new HtmlDocument();
             doc.Load(args[0]);
 
             //var nodes = doc.DocumentNode.Descendants("img").Select(y => y.Descendants().Where(x => x.Attributes["id"].Value.StartsWith("Panorama"))).ToList();
@@ -22,19 +24,19 @@ namespace grabber
 
             //foreach(var i in a)
             List<string> files = new List<string>();
-            for(int i =0; i < a.Count; i ++)
+            for (int i = 0; i < a.Count; i++)
             {
                 Base64ToImage(a[i].Attributes["id"].Value + ".jpg", a[i].Attributes["src"].Value);
                 files.Add(a[i].Attributes["id"].Value + ".jpg");
                 //Console.WriteLine(i.Attributes["src"].Value);
             }
             var vmp = CombineBitmap(files.ToArray());
-            vmp.Save("final.jpg",ImageFormat.Jpeg);
-            
+            vmp.Save("final.jpg", ImageFormat.Jpeg);*/
+
         }
         public static void Base64ToImage(string fileName, string b64)
         {
-            byte[] img = Convert.FromBase64String(b64.Remove(0,23));
+            byte[] img = Convert.FromBase64String(b64.Remove(0, 23));
             MemoryStream ms = new MemoryStream(img, 0, img.Length);
 
             ms.Write(img, 0, img.Length);
@@ -42,7 +44,114 @@ namespace grabber
             Console.WriteLine(String.Format("{0}: {1}x{2}", fileName, image.Height, image.Width));
             image.Save(fileName);
         }
+        public static byte[] ConvertToEquirecTangular(Bitmap input, int outWidth, int outHeight)
+        {
+            Bitmap bitmap = new Bitmap(outWidth, outHeight, PixelFormat.Format24bppRgb);
+            float u, v;
+            float phi, theta;
+            int cubeFaceWidth, cubeFaceHeight;
 
+            cubeFaceWidth = input.Width / 4;
+            cubeFaceHeight = input.Height / 3;
+
+            for (int j = 0; j < bitmap.Height; j++)
+            {
+                v = 1 - ((float)j / bitmap.Height);
+                theta = v * (float)Math.PI;
+
+                for(int i = 0; i < bitmap.Width; i++)
+                {
+                    u = ((float)i / bitmap.Width);
+                    phi = u * 2 * (float)Math.PI;
+
+                    float x, y, z;
+                    x = (float)Math.Sin(phi) * (float)Math.Sin(theta) * -1;
+                    y = (float)Math.Cos(theta);
+                    z = (float)Math.Cos(phi) * (float)Math.Sin(theta) * -1;
+
+                    float xa, ya, za;
+                    float a;
+
+                    a = Math.Max(Math.Abs(x), Math.Max(Math.Abs(y), Math.Abs(z)));
+                    xa = x / a;
+                    ya = y / a;
+                    za = z / a;
+                    Color color;
+                    int xPixel, yPixel;
+                    int xOffset, yOffset;
+
+                    if (xa == 1)
+                    {
+                        //Right
+                        xPixel = (int)((((za + 1f) / 2f) - 1f) * cubeFaceWidth);
+                        xOffset = 2 * cubeFaceWidth; //Offset
+                        yPixel = (int)((((ya + 1f) / 2f)) * cubeFaceHeight);
+                        yOffset = cubeFaceHeight; //Offset
+                    }
+                    else if (xa == -1)
+                    {
+                        //Left
+                        xPixel = (int)((((za + 1f) / 2f)) * cubeFaceWidth);
+                        xOffset = 0;
+                        yPixel = (int)((((ya + 1f) / 2f)) * cubeFaceHeight);
+                        yOffset = cubeFaceHeight;
+                    }
+                    else if (ya == 1)
+                    {
+                        //Up
+                        xPixel = (int)((((xa + 1f) / 2f)) * cubeFaceWidth);
+                        xOffset = cubeFaceWidth;
+                        yPixel = (int)((((za + 1f) / 2f) - 1f) * cubeFaceHeight);
+                        yOffset = 2 * cubeFaceHeight;
+                    }
+                    else if (ya == -1)
+                    {
+                        //Down
+                        xPixel = (int)((((xa + 1f) / 2f)) * cubeFaceWidth);
+                        xOffset = cubeFaceWidth;
+                        yPixel = (int)((((za + 1f) / 2f)) * cubeFaceHeight);
+                        yOffset = 0;
+                    }
+                    else if (za == 1)
+                    {
+                        //Front
+                        xPixel = (int)((((xa + 1f) / 2f)) * cubeFaceWidth);
+                        xOffset = cubeFaceWidth;
+                        yPixel = (int)((((ya + 1f) / 2f)) * cubeFaceHeight);
+                        yOffset = cubeFaceHeight;
+                    }
+                    else if (za == -1)
+                    {
+                        //Back
+                        xPixel = (int)((((xa + 1f) / 2f) - 1f) * cubeFaceWidth);
+                        xOffset = 3 * cubeFaceWidth;
+                        yPixel = (int)((((ya + 1f) / 2f)) * cubeFaceHeight);
+                        yOffset = cubeFaceHeight;
+                    }
+                    else
+                    {
+                        xPixel = 0;
+                        yPixel = 0;
+                        xOffset = 0;
+                        yOffset = 0;
+                    }
+
+                    xPixel = Math.Abs(xPixel);
+                    yPixel = Math.Abs(yPixel);
+                    xPixel += xOffset;
+                    yPixel += yPixel;
+
+                    
+                    color = input.GetPixel(xPixel, yPixel);
+                    bitmap.SetPixel(i, j, color);
+                    
+                }
+                
+            }
+            bitmap.Save("out.jpg", ImageFormat.Jpeg);
+            ImageConverter imageConverter = new ImageConverter();
+            return (byte[])imageConverter.ConvertTo(bitmap, typeof(byte[]));
+        }
         public static System.Drawing.Bitmap CombineBitmap(string[] files)
         {
             //read all images into memory
@@ -54,7 +163,7 @@ namespace grabber
                 int width = 0;
                 int height = 0;
 
-                foreach (string image in files )
+                foreach (string image in files)
                 {
                     //create a Bitmap from the file and add it to the list
                     System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(image);
